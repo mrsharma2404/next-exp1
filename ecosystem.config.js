4a7379b0fbf03d30b645017ea97c9026728c3255
mrsharma2404 <mrsharma2404@egmail.com>
2024-12-19 14:42:34 +0530

    updated config for our repo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,8 +15,8 @@ module.exports = {
       user: "ubuntu",
       host: "az-staging-2-partners.thrillo.dev",
       ref: "origin/main",
-      repo: "git@github.com:thrillophilia/Thrillophilia-Next.git",
-      path: "/home/ubuntu/apps/thrillophilia-next",
+      repo: "git@github.com:mrsharma2404/next-exp1.git",
+      path: "/home/ubuntu/apps/next-exp1",
       "pre-deploy-local": "",
       "post-deploy":
         "source ~/.nvm/nvm.sh && nvm use 20.5.1 && npm install && npm run build && pm2 reload ecosystem.config.js --env staging --update-env",
